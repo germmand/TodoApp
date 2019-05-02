9aebb3f8fcd91ef8a04a1329cff67aa526a710f7
germmand <german.aguilera17@gmail.com>
2019-05-02 17:23:54 -0400

    test: Added tests for UserListRegistrationAPIView.

--- api/Authentication/tests.py
+++ api/Authentication/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+from Authentication.models import User
+
+class UserListRegistrationAPIViewTests(APITestCase):
+    def test_return_list_of_users_correctly(self):
+        url = reverse('user-list')
+        data = {
+            'username': 'DummyUser',
+            'email': 'dummy@email.com',
+            'password': 'superpassword123'
+        }
+        User.objects.create(**data)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), User.objects.count())
+        self.assertEqual(response.data[0]['username'], User.objects.get().username)
+
+    def test_create_account_successfully(self):
+        url = reverse('user-list')
+        data = {
+            'username': 'DummyUser',
+            'email': 'dummy@email.com',
+            'password': 'superpassword123'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, data['username'])
+
+    def test_return_400_when_user_already_exists(self):
+        url = reverse('user-list')
+        data = {
+            'username': 'DummyUser',
+            'email': 'dummy@email.com',
+            'password': 'superpassword123'
+        }
+        User.objects.create(**data)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
