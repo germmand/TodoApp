ef674a9c406b002a637ad8631439e07b785055d9
germmand <german.aguilera17@gmail.com>
2019-05-05 13:50:45 -0400

    feat: Adding PostgreSQL database provider.

--- api/TodoApp/settings.py
+++ api/TodoApp/settings.py
@@ -93,6 +93,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresdb': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'), # 'db' defaults from docker service.
+        'POST': os.getenv('POSTGRES_PORT', 5432),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '')
     }
 }
 
