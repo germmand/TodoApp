e33f3357a1a192cb4af05b481c30637d72e68736
germmand <german.aguilera17@gmail.com>
2019-05-09 10:45:44 -0400

    feat: Simple React Set up for web application.

---
+++ webapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Hey, there!</h2>
+                <hr />
+                <h3>You set this up well-enough.</h3>
+            </div>
+        );
+    }
+}
+
+export default App;
