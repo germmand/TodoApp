6999a08c35b55d32acebb8807487efb865d4aa52
germmand <german.aguilera17@gmail.com>
2019-05-10 13:33:44 -0400

    build: Adds Material-UI toolkit.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 const App = () => (
     <div>
         <h2>Hey, there!</h2>
         <hr />
         <h3>You set this up pretty good.</h3>
+        <Button
+          variant="contained"
+          color="primary"
+        >
+            Click me!
+        </Button>
     </div>
 );
 
--- webapp/src/containers/Root.js
+++ webapp/src/containers/Root.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import App from '../App';
 
+// I'm going to leave this here
+// just in case I want to change it.
+const theme = createMuiTheme();
+
 const Root = ({ store }) => (
     <Provider store={store}>
-        <Router>
-            <Route
-                path="/"
-                component={App}
-            />
-        </Router>
+        <MuiThemeProvider theme={theme}>
+            <Router>
+                <Route
+                    path="/"
+                    component={App}
+                />
+            </Router>
+        </MuiThemeProvider>
     </Provider>
 );
 
